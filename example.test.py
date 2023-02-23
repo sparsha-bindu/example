31819022a177b2765b30dee89cb7236a64877be8
sparsha-bindu <85055472+sparsha-bindu@users.noreply.github.com>
2023-02-23 18:08:34 +0530

    Update example.test.py

--- example.test.py
+++ example.test.py
@@ -1,6 +1,6 @@
 import example
 
 def test_add():
-    assert main.add(2, 3) == 5
-    assert main.add(0, 0) == 0
-    assert main.add(-1, 1) == 0
+    assert example.add(2, 3) == 5
+    assert example.add(0, 0) == 0
+    assert example.add(-1, 1) == 0
