085dae60aa840c3bc8a1ac241305ece4248d25aa
sparsha-bindu <85055472+sparsha-bindu@users.noreply.github.com>
2023-03-24 12:16:34 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,2 +1,8 @@
-def add(a, b):
-    return a + b
+def find_max(numbers):
+    if not numbers:
+        return None
+    max_num = numbers[0]
+    for num in numbers:
+        if num > max_num:
+            max_num = num
+    return max_num
